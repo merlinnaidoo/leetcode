2da06b529695a36bab1f841525ea95453091ee8d
Merlin Naidoo <naidoomerlin4@gmail.com>
2020-06-12 18:33:03 +0200

    finally dum mistakes

--- twoSum.js
+++ twoSum.js
@@ -3,22 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-let arr = [2 ,7, 11, 15]
-var twoSum = function(nums, target) {
-    let ansArr = [];
-    let index1 = 0;
-    let index2 = 0;
-    
-    for (let i = 0; i <= nums.length; i++){
-        index1 = i;
-        index2 = i+1;
-    
-        if ((nums[index1] + nums[index2]) == target){
-            ansArr.push(index1);
-            ansArr.push(index2);
-        }
-        return ansArr;
+var twoSum = (nums, target) => {
+    for (let x = 0; x < nums.length; x++){
+        for (let i = x + 1 ; i < nums.length; i++){
+            if (nums[x] + nums[i] == target){
+                return iArr = [x,i];
+            }
+        }    
     }
 };
 
-console.log(twoSum(arr,9));
+console.log(twoSum([2,5,5,11],10));
+
